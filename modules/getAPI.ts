050b0f5dad2f1558ca5b47e8ff979da9c94e8a94
Hyeokjae Lee (Nagle) <71566740+HyeokjaeLee@users.noreply.github.com>
2021-05-23 07:06:50 +0900

    clean up

--- modules/getAPI.ts
+++
@@ -1,7 +0,0 @@
-export const getJSON_Array = async (url: string) => {
-  const data = await fetch(url);
-  console.log(data);
-  const test_ = await data.json();
-  console.log(test_);
-  return test_;
-};
