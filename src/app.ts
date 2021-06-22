ea6c353753b15d3cd7d383e4b6f8355a9570169d
Hyeokjae Lee (Nagle) <71566740+HyeokjaeLee@users.noreply.github.com>
2021-06-22 15:59:30 +0900

    ver 1.2.1
    change for heroku

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import graphqlRouter from "./routes/graphql";
 const exp = express();
 
-exp.listen(8080, () => {
+exp.listen(process.env.PORT || 8080, () => {
   console.log("Server listening on port 8080");
 });
 
