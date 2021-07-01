406aaf047a61474233f81da2df6a38b15dcdceed
Hyeokjae Lee (Nagle) <71566740+HyeokjaeLee@users.noreply.github.com>
2021-07-02 06:23:20 +0900

    [ADD] Region Enum
    Add Region Enum to query.

---
+++ src/schema/region-enum.ts
@@ -0,0 +1,22 @@
+export const Region = `
+    enum Region {
+      Seoul
+      Total
+      Gyeonggido
+      Daegu
+      Incheon
+      Busan
+      Ulsan
+      Daejeon
+      Gwangju
+      Sejong
+      Chungcheongnamdo
+      Chungcheongbukdo
+      Gyeongsangnamdo
+      Gyeongsangbukdo
+      Jeollanamdo
+      Jeollabukdo
+      Gangwondo
+      Jeju
+      Lazaretto
+    }`;
