53fe9a3e86decd5ea9d9c4c7a26f10e63c2f19cf
Hyeokjae Lee (Nagle) <71566740+HyeokjaeLee@users.noreply.github.com>
2021-07-02 06:20:00 +0900

    [ADD] Region Enum
    Add Region Enum to query

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,12 @@ import create_data from "./components/create-data";
 import { covid19Schema } from "./schema/covid19-schema";
 import { date2query_form } from "./function/convert-format";
 import cors from "cors";
+import { Region } from "./schema/region-enum";
 const port = process.env.PORT || 8080,
   schema = buildSchema(`
+    ${Region}
     type Query {
-      covid19Info(region: String, startDate: Int, endDate: Int): [DataSet]
+      covid19Info(region: Region, startDate: Int, endDate: Int): [DataSet]
     }
     ${covid19Schema}
   `);
@@ -34,7 +36,7 @@ create_data().then((data) => {
         const covid19Info = region!
           ? [data.find((value) => value.regionEng === region)]
           : data;
-
+        //forEach로 covid19Info 객체 수정 시 Deep Copy가 아니기 때문에 상위 Data가 수정됨
         const result = covid19Info.map((_covid19Info) => {
           const _covid19 = _covid19Info?.covid19.filter((_covid19) => {
             const dateNum = date2query_form(_covid19.date);
