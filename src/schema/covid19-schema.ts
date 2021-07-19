c90cfdf00b8a25625b3adef11073cbb50163a4ad
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2021-07-20 02:39:50 +0900

    Refactoring: 파편화 되있던 스키마 통합

--- src/schema/covid19-schema.ts
+++ src/schema/covid19-schema.ts
@@ -1,47 +1,73 @@
 export const covid19Schema = `
-    type RegionalData {
-      regionEng: String
-      regionKor: String
-      population: Int
-      distancingLevel: Int
-      covid19DataList: [Covid19DataList]
-    }
+  enum Region {
+    Seoul
+    Total
+    Gyeonggido
+    Daegu
+    Incheon
+    Busan
+    Ulsan
+    Daejeon
+    Gwangju
+    Sejong
+    Chungcheongnamdo
+    Chungcheongbukdo
+    Gyeongsangnamdo
+    Gyeongsangbukdo
+    Jeollanamdo
+    Jeollabukdo
+    Gangwondo
+    Jeju
+    Lazaretto
+  }
 
-    type Covid19DataList {
-      date: String
-      confirmed: Confirmed
-      quarantine: Quarantine
-      recovered: BasicStructure
-      dead: BasicStructure
-      vaccinated: Vaccinated
-      per100kConfirmed: Float
-      immunityRatio: Float
-    }
+  type Query {
+    regionalDataList(region: Region, startDate: Int, endDate: Int, onlyLastDate: Boolean): [RegionalData]
+  }
 
-    type BasicStructure {
-      total: Int
-      new: Int
-      accumlated: Int
-    }
+  type RegionalData {
+    regionEng: String
+    regionKor: String
+    population: Int
+    distancingLevel: Int
+    covid19DataList: [Covid19DataList]
+  }
 
-    type Confirmed {
-      total: Int
-      accumlated: Int
-    }
+  type Covid19DataList {
+    date: String
+    confirmed: Confirmed
+    quarantine: Quarantine
+    recovered: BasicStructure
+    dead: BasicStructure
+    vaccinated: Vaccinated
+    per100kConfirmed: Float
+    immunityRatio: Float
+  }
 
-    type Quarantine {
-      total: Int
-      new: QuarantineNew
-    }
+  type BasicStructure {
+    total: Int
+    new: Int
+    accumlated: Int
+  }
 
-    type QuarantineNew {
-      total: Int
-      domestic: Int
-      overseas: Int
-    }
+  type Confirmed {
+    total: Int
+    accumlated: Int
+  }
 
-    type Vaccinated   {
-      first: BasicStructure
-      second: BasicStructure
-    }
+  type Quarantine {
+    total: Int
+    new: QuarantineNew
+  }
+
+  type QuarantineNew {
+    total: Int
+    domestic: Int
+    overseas: Int
+  }
+  
+  type Vaccinated   {
+    first: BasicStructure
+    second: BasicStructure
+  }
 `;
