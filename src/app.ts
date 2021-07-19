4bd23bf721d7fb2841a70a62423755fdd246b62a
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2021-07-19 23:55:43 +0900

    perf: 코드 구조 변경
    * 요청받은 데이터 필터링 방식 변경
    
    * 코드 구조 개선
    
    BREAKING CHANGE: 컴포넌트 개선에 따른 전체 파일 구조 변경 필요.

--- src/app.ts
+++ src/app.ts
@@ -1,73 +1,73 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import { buildSchema } from "graphql";
-import create_data from "./components/create-data";
+import { COVID19 } from "./components/create-COVID19-data";
 import { covid19Schema } from "./schema/covid19-schema";
 import { date2query_form } from "./function/convert-format";
 import type { RegionalData } from "./types/data-type";
 import cors from "cors";
+import clone from "fast-copy";
 import { Region } from "./schema/region-enum";
-const port = process.env.PORT || 8080,
-  schema = buildSchema(`
+
+async function app() {
+  const covid19 = new COVID19(),
+    exp = express(),
+    port = process.env.PORT || 8080;
+  await covid19.update();
+  const schema = buildSchema(`
     ${Region}
     type Query {
       regionalDataList(region: Region, startDate: Int, endDate: Int, onlyLastDate: Boolean): [RegionalData]
     }
     ${covid19Schema}
   `);
-create_data().then((data) => {
-  const update_data = (second: number) => {
-      setInterval(() => {
-        try {
-          console.log(new Date());
-          create_data().then((_data) => {
-            data = _data;
-          });
-          console.log("Data Update Successful");
-        } catch (e) {
-          console.log(`Data update failed : ${e}`);
+  setInterval(() => {
+    covid19.update();
+  }, 600000);
+  const root = {
+    regionalDataList: (args: any, context: any, info: any) => {
+      let { region, startDate, endDate, onlyLastDate } = args;
+      let regionalDataList: RegionalData[];
+      {
+        if (!region && !startDate && !endDate && !onlyLastDate) {
+          regionalDataList = covid19.data;
+        } else if (!!region) {
+          regionalDataList = [
+            <RegionalData>(
+              clone(
+                covid19.data.find(
+                  (covid19Data) => covid19Data.regionEng === region
+                )
+              )
+            ),
+          ];
+        } else {
+          regionalDataList = clone(covid19.data);
         }
-      }, second * 1000);
+
+        if (!!startDate || !!endDate) {
+          startDate = startDate! ? startDate : 0;
+          endDate = endDate! ? endDate : date2query_form(new Date());
+          regionalDataList.forEach((regionalData) => {
+            regionalData.covid19DataList = regionalData.covid19DataList.filter(
+              (covid19Data) => {
+                const numDate = date2query_form(covid19Data.date);
+                return numDate >= startDate && numDate <= endDate;
+              }
+            );
+          });
+        }
+
+        if (onlyLastDate) {
+          regionalDataList.forEach((regionalData) => {
+            regionalData.covid19DataList =
+              regionalData.covid19DataList.slice(-1);
+          });
+        }
+      }
+      return regionalDataList;
     },
-    root = {
-      regionalDataList: (args: any, context: any, info: any) => {
-        let { region, startDate, endDate, onlyLastDate } = args;
-        startDate = startDate! ? startDate : 0;
-        endDate = endDate! ? endDate : date2query_form(new Date());
-        const regionalFilteredDataList = region!
-            ? [<RegionalData>data.find((value) => value.regionEng === region)]
-            : data,
-          //forEach로 covid19Info 객체 수정 시 Deep Copy가 아니기 때문에 상위 Data가 수정됨
-          dateFilteredDataList = regionalFilteredDataList.map(
-            (regionalFilteredData) => {
-              const dateFilteredCovid19DataList =
-                regionalFilteredData.covid19DataList.filter((covid19Data) => {
-                  const dateNum = date2query_form(covid19Data.date);
-                  return dateNum >= startDate && dateNum <= endDate;
-                });
-              return {
-                regionKor: regionalFilteredData.regionKor,
-                regionEng: regionalFilteredData.regionEng,
-                population: regionalFilteredData.population,
-                covid19DataList: dateFilteredCovid19DataList,
-              };
-            }
-          );
-        return onlyLastDate === true
-          ? dateFilteredDataList.map((dateFilteredData) => ({
-              regionKor: dateFilteredData.regionKor,
-              regionEng: dateFilteredData.regionEng,
-              population: dateFilteredData.population,
-              covid19DataList: [
-                dateFilteredData.covid19DataList[
-                  dateFilteredData.covid19DataList.length - 1
-                ],
-              ],
-            }))
-          : dateFilteredDataList;
-      },
-    },
-    exp = express();
+  };
   exp.listen(port, () => {
     console.log(`Server listening on port ${port}`);
   });
@@ -80,5 +80,6 @@ create_data().then((data) => {
       graphiql: true,
     })
   );
-  update_data(600);
-});
+}
+
+app();
