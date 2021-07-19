41e83f333466fe0e1ad7e4d98e26753ad85d147b
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2021-07-19 18:36:09 +0900

    [UPDATE] create-COVID19-data.ts
    * update method 추가
    
    * 연산도중 값 조회 대응 코드 추가
    
    * 전반적인 코드 Refactoring
    
    * 주석 내용 변경

--- src/components/create-COVID19-data.ts
+++ src/components/create-COVID19-data.ts
@@ -17,81 +17,74 @@ import { get_distancing_data } from "./get-distancing-data";
 const date_formatter = (originalDate: string) =>
   convert_date_format(new Date(originalDate), "-");
 
-/**
- * 공공데이터 포털의 소스 데이터를 불러옴
- * @returns 확진 정보와 예방접종 정보를 가진 Object
- * const getSourcData = () =>
-  Promise.all([get_confirmed_data(), get_vaccine_data()]).then(
-    (sourceData) => ({
-      confirmed: sourceData[0],
-      vaccine: sourceData[1],
-    })
-  );
- */
-
-/**
- * 여러가지 COVID19 관련 소스 데이터들을 가공해 지역별 데이터를 만듦
- * @param distancingDataList 거리두기 소스 데이터
- * @param infectionDataList 감염 소스 데이터
- * @param vaccinationDataList 예방접종 소스 데이터
- */
-class COVID19Data {
-  /**
-   * COVID19Data Object Key
-   *  - date: 데이터 생성 일자
-   * - confirmed: 확진
-   * - quarantine: 격리
-   * - recovered: 격리해제(회복)
-   * - dead: 사망
-   * - vaccinated: 백신 접종
-   * - per100kConfirmed: 10만명 당 확진자 수
-   * - immunityRatio: 면역자 비율(2차 백신 접종 + 회복)
-   *
-   * 공통 하위 Object Key
-   * - total: 전체(신규+누적)
-   * - new: 신규(당일)
-   * - accumlated: 누적(전일)
-   *
-   * new quarantine(신규 격리자) 하위 Object Key
-   * - domestic: 국내 감염
-   * - overseas: 해외 감염
-   *
-   * vaccinated 하위 Object Key
-   * - first: 1차 접종
-   * - second: 2차 접종
+/**COVID19 정보 생성을 위한 클래스*/
+export class COVID19 {
+  /**여러가지 소스데이터를 가져오기
+   * - 비동기 함수
+   * @returns 감염, 거리두기, 예방접종 데이터를 가지고 있는 Object
    */
-  public regionalDataList: RegionalData[];
-  constructor(
-    distancingDataList: DistancingSourceData[],
-    infectionDataList: InfectionSourceData[],
-    vaccinationDataList: VaccinationSourceData[]
-  ) {
-    //기본 데이터 구조를 담을 빈 배열과 거리두기 단계를 추가한 지역별 데이터를 저장
-    this.regionalDataList = regionInfo.map((regionalData: any) => {
-      const distancingData = distancingDataList.find(
-        (distancingData) => distancingData.region === regionalData.regionKor
-      );
-      regionalData.distancingLevel =
-        distancingData != undefined ? distancingData.distancingLevel : null;
+  private static get_source_data = () =>
+    Promise.all([
+      get_infection_data(),
+      get_distancing_data(),
+      get_vaccination_data(),
+    ]).then((sourceData) => ({
+      infection: sourceData[0],
+      distancing: sourceData[1],
+      vaccination: sourceData[2],
+    }));
+
+  /**
+   * COVID19 관련 지역별 정보 Array
+   * - 연산중 값 조회를 방지하기 위해 temp에서 연산이 모두 종료된 후 값이 변경됨
+   */
+  public data: RegionalData[] = [];
+  /**실제 연산이 이루어지는 임시 변수*/
+  private temp: RegionalData[] = [];
+
+  /**동적 영역 초기화*/
+  private readonly init = () => {
+    this.temp = regionInfo.map((regionalData: any) => {
+      regionalData.distancingLevel = null;
       regionalData.covid19DataList = [];
       return regionalData;
     });
-    this.classify_by_region(infectionDataList);
-    this.combine_vaccinationData(vaccinationDataList);
-    this.create_additionalData();
-  }
+  };
 
   /**
-   * 소스 데이터를 기반으로 지역별 기초 데이터 생성
-   * @param infectionSourceDataList 공공데이터의 감염 소스 데이터
-   * @param distancingSourceDataList 공공데이터의 거리두기 소스 데이터
-   * @returns 지역별 기초 데이터
+   * temp 초기화 후 소스 데이터들을 가공해 지역별 COVID19 데이터 생성
+   * @param distancingDataList 사회적 거리두기 소스 데이터
+   * @param infectionDataList 감염 소스 데이터
+   * @param vaccinationDataList 예방접종 소스 데이터
    */
-  private classify_by_region(infectionSourceDataList: InfectionSourceData[]) {
+  public update = async () => {
+    try {
+      console.log("\n" + new Date());
+      const sourceData = await COVID19.get_source_data();
+      this.init();
+      this.classify_by_region(sourceData.infection, sourceData.distancing);
+      this.combine_vaccinationData(sourceData.vaccination);
+      this.create_additionalData();
+      this.data = this.temp;
+      console.log("Data Update Successful");
+    } catch (err) {
+      console.log(`Data update failed: ${err}`);
+    }
+  };
+
+  /**
+   * 소스 데이터를 가공하여 지역별로 분류하여 temp에 저장
+   * @param infectionDataList 감염 소스 데이터
+   * @param distancingDataList 사회적 거리두기 소스 데이터
+   */
+  private classify_by_region(
+    infectionSourceDataList: InfectionSourceData[],
+    distancingDataList: DistancingSourceData[]
+  ) {
     /**
-     * 감염 정보 소스 데이터를 기반으로 기본적인 데이터 구조 생성
-     * @param confirmedSourceData 확진 정보 소스 데이터
-     * @returns 확진 정보 소스 데이터 기반 기초 데이터 구조
+     * 감염과 거리두기 소스 데이터를 기반으로 소단위 기본적 데이터 구조 생성
+     * @param infectionSourceData 감염 소스 데이터
+     * @returns 감염 소스 데이터 기반 기초 데이터 구조
      */
     const create_basicStructure = (
       infectionSourceData: InfectionSourceData
@@ -139,83 +132,78 @@ class COVID19Data {
     });
     /** 확진 정보 소스 데이터를 지정한 데이터 구조에 담아 지역별로 구분*/
     infectionSourceDataList.forEach((infectionSourceData) => {
-      const regionIndex = this.regionalDataList.findIndex(
+      const regionIndex = this.temp.findIndex(
         (regionalData) =>
           regionalData.regionEng ==
           infectionSourceData.gubunEn._text.replace("-", "")
       );
-      this.regionalDataList[regionIndex].covid19DataList.push(
+      this.temp[regionIndex].covid19DataList.push(
         create_basicStructure(infectionSourceData)
       );
     });
-    this.regionalDataList.forEach((regionalData) => {
-      regionalData.covid19DataList = regionalData.covid19DataList.reverse();
+    //루프 최소화를 위해 같이 진행
+    this.temp.forEach((regionalData) => {
+      const distancingData = distancingDataList.find(
+        (distancingData) => distancingData.region === regionalData.regionKor
+      );
+      //거리두기 정보 추가
+      regionalData.distancingLevel =
+        distancingData != undefined ? distancingData.distancingLevel : null;
+      regionalData.covid19DataList = regionalData.covid19DataList.reverse(); //배열에 넣어둔 날짜가 역순이므로 재배치
     });
   }
 
   /**
-   * 분류된 지역별 데이터에 백신 데이터를 추가
+   * 기존 temp에 백신 데이터를 추가하여 저장
    * @param vaccinationSourceDataList 예방접종 소스 데이터
-   * @param regionalDataList 소스 데이터를 기반으로 생성된 지역별 데이터
-   * @returns 예방접종 데이터가 추가된 지역별 데이터
    */
   private combine_vaccinationData(
     vaccinationSourceDataList: VaccinationSourceData[]
   ) {
     vaccinationSourceDataList.forEach((vaccinationSourceData) => {
-      try {
-        const regionIndex = this.regionalDataList.findIndex(
-          (regionalData) =>
-            regionalData.regionKorFull === vaccinationSourceData.sido
+      const regionIndex = this.temp.findIndex(
+        (regionalData) =>
+          regionalData.regionKorFull === vaccinationSourceData.sido
+      );
+      //지역 구분 '기타'는 예방접종 소스 데이터에는 있지만 확진 정보 소스 데이터에는 없으므로 제외
+      if (regionIndex != -1) {
+        const DateIndex = this.temp[regionIndex].covid19DataList.findIndex(
+          (covid19Data) =>
+            covid19Data.date == date_formatter(vaccinationSourceData.baseDate)
         );
-        //지역 구분 '기타'는 예방접종 소스 데이터에는 있지만 확진 정보 소스 데이터에는 없으므로 제외
-        if (regionIndex != -1) {
-          const DateIndex = this.regionalDataList[
-            regionIndex
-          ].covid19DataList.findIndex(
-            (covid19Data) =>
-              covid19Data.date == date_formatter(vaccinationSourceData.baseDate)
-          );
-          if (DateIndex != -1) {
-            /** 전체 데이터 구조에서 추가할 값들에 쉽게 접근 하기위한 shallow copy*/
-            const covid19Data =
-                this.regionalDataList[regionIndex].covid19DataList[DateIndex],
-              vaccinationData = covid19Data.vaccinated;
-            //예방접종 데이터 추가
-            {
-              vaccinationData.first.total = vaccinationSourceData.totalFirstCnt;
-              vaccinationData.first.new = vaccinationSourceData.firstCnt;
-              vaccinationData.first.accumlated =
-                vaccinationSourceData.accumulatedFirstCnt;
-              vaccinationData.second.total =
-                vaccinationSourceData.totalSecondCnt;
-              vaccinationData.second.new = vaccinationSourceData.secondCnt;
-              vaccinationData.second.accumlated =
-                vaccinationSourceData.accumulatedSecondCnt;
-            }
-            //면역 비율 데이터 추가
-            covid19Data.immunityRatio =
-              Math.round(
-                ((vaccinationData.second.total +
-                  <number>covid19Data.recovered.total) /
-                  <number>this.regionalDataList[regionIndex].population) *
-                  1000
-              ) / 1000;
+        if (DateIndex != -1) {
+          /** 전체 데이터 구조에서 추가할 값들에 쉽게 접근 하기위한 shallow copy*/
+          const covid19Data = this.temp[regionIndex].covid19DataList[DateIndex],
+            vaccinationData = covid19Data.vaccinated;
+          //예방접종 데이터 추가
+          {
+            vaccinationData.first.total = vaccinationSourceData.totalFirstCnt;
+            vaccinationData.first.new = vaccinationSourceData.firstCnt;
+            vaccinationData.first.accumlated =
+              vaccinationSourceData.accumulatedFirstCnt;
+            vaccinationData.second.total = vaccinationSourceData.totalSecondCnt;
+            vaccinationData.second.new = vaccinationSourceData.secondCnt;
+            vaccinationData.second.accumlated =
+              vaccinationSourceData.accumulatedSecondCnt;
           }
+          //면역 비율 데이터 추가
+          covid19Data.immunityRatio =
+            Math.round(
+              ((vaccinationData.second.total +
+                <number>covid19Data.recovered.total) /
+                <number>this.temp[regionIndex].population) *
+                1000
+            ) / 1000;
         }
-      } catch (error) {
-        console.log("combine_vaccine_data : " + error);
       }
     });
   }
 
   /**
-   * 분류된 데이터들로 유추 가능한 추가적인 데이터들을 생성
-   * @param combined_regionalDataList 예방접종 데이터가 추가된 지역별 데이터
-   * @returns 최종 지역별 COVID19 정보 데이터
+   * temp의 여러 값들을 기반으로 유추가능한 추가적인 데이터 생성 및 추가 저장
    */
   private create_additionalData() {
-    this.regionalDataList.forEach((regionalData) => {
+    this.temp.forEach((regionalData) => {
       regionalData.covid19DataList.forEach((covid19Data, index) => {
         if (index != 0) {
           const covid19Data_1dayAgo = regionalData.covid19DataList[index - 1];
