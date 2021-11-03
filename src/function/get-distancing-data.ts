d649d24a5e87a001467619b5c044c0484bc49bd8
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2021-11-03 09:04:27 +0900

    ⚰️ refactor: request 코드 제거
    * 모든 코드 axios로 대체 완료
    * 거리두기 파싱 코드 정규표현식으로 변경

--- src/function/get-distancing-data.ts
+++ src/function/get-distancing-data.ts
@@ -1,49 +1,36 @@
-import request from "request";
+import axios from "axios";
 import type { DistancingSourceData } from "../types/data-type";
 
 /**
  * 한국 사회적 거리두기 현황 사이트 원본 소스를 가져와 파싱후 반환
  * @returns 지역명과 거리두기 단계를 가진 Object Array
- * * cheerio로 크롤링 해서 사용하려 했으나 해당 사이트의 작동방식 때문에 사용이 불가능해서 string 형식으로 파싱하여 사용함
+ * * 주석에 지역명이 있기 때문에 string 형식으로 파싱하여 사용함
  */
-export const get_distancing_data = (): Promise<DistancingSourceData[]> =>
-  new Promise((resolve, reject) => {
-    request("http://ncov.mohw.go.kr/regSocdisBoardView.do", (error, response, body) => {
-      /**지역 구분 '전국'의 거리두기 단계를 전지역 평균으로 지정하기 위한 계산을 위해 모든 지역의 거리두기 단계를 더한 값*/
-      let distancingLevelSum = 0;
-      let sourceData: string = response.body;
-      /**원본 소스 script태그의 거리두기 데이터가 있는 위치의 시작 문자열*/
-      const startBaseStringValue = "RSS_DATA = [";
-      const dataStartIndex = sourceData.indexOf(startBaseStringValue) + startBaseStringValue.length;
-      const dataEndIndex = sourceData.indexOf("]", dataStartIndex);
-      //거리두기 데이터가 있는 문자열 저장 & 공백 제거
-      sourceData = sourceData.substring(dataStartIndex, dataEndIndex).replace(/\s/g, "");
-
-      /**
-       * 단순 string 값을 지역 단위로 나누어 놓은 배열
-       ** '{caution'으로 문자열이 시작하기 때문에 첫 값 제외*/
-      const splitSourceDataList = sourceData.split("{caution").splice(1);
-      /**지역과 거리두기 단계를 가지는 Object Array*/
-      const distancingDataList = splitSourceDataList.map((splitSourceData) => {
-        /**거리두기 단계를 가지는 문자열의 시작 위치*/
-        const distancingLevelIndex = splitSourceData.indexOf("value") + 7;
-        /**지역 이름을 가지는 문자열의 시작 위치*/
-        const regionIndex = splitSourceData.indexOf("//") + 2;
-        /**거리두기 단계*/
-        const distancingLevel = Number(splitSourceData.substr(distancingLevelIndex, 1));
-        distancingLevelSum += distancingLevel;
-        return {
-          region: splitSourceData.substr(regionIndex, 2),
-          distancingLevel: distancingLevel,
-        };
-      });
-
-      // 지역 구분 '전국' Object를 추가
-      distancingDataList.push({
-        region: "전국",
-        //전지역의 평균을 소숫점 첫째 자리에서 반올림 한 값을 거리두기 단계로 지정
-        distancingLevel: Math.round((distancingLevelSum / distancingDataList.length) * 10) / 10,
-      });
-      resolve(distancingDataList);
-    });
+export async function get_distancing_data(): Promise<DistancingSourceData[]> {
+  const sourceData = await axios.get("http://ncov.mohw.go.kr/regSocdisBoardView.do").then((res) => {
+    const data: string = res.data;
+    const startString = "RSS_DATA = [";
+    const startIndex = data.indexOf(startString) + startString.length;
+    const endIndex = data.indexOf("]", startIndex);
+    return data.substring(startIndex, endIndex).replace(/\s/g, "");
   });
+
+  let distancingLevelSum = 0;
+  const splitSourceDataList = sourceData.split("{caution").splice(1);
+  /**지역과 거리두기 단계를 가지는 Object Array*/
+  const distancingDataList = splitSourceDataList.map((splitSourceData) => {
+    const region = splitSourceData.match(/(?<=\/\/)[^0-9]+/)![0];
+    const distancingLevel = Number(splitSourceData.match(/(?<=value:')[0-9]/)![0]);
+    distancingLevelSum += distancingLevel;
+    return {
+      region: region,
+      distancingLevel: distancingLevel,
+    };
+  });
+  // 지역 구분 '전국'은 모든 지역의 평균 거리두기 값
+  distancingDataList.push({
+    region: "전국",
+    distancingLevel: Math.round((distancingLevelSum / distancingDataList.length) * 10) / 10,
+  });
+  return distancingDataList;
+}
--- src/function/get-external-data.ts
+++ src/function/get-external-data.ts
@@ -1,7 +1,4 @@
-import * as convert from "xml-js";
-import request from "request";
 import { Worker } from "worker_threads";
-import axios from "axios";
 export const get_data_from_worker = (dir: string): any => {
   return new Promise(function (resolve, reject) {
     const worker = new Worker(dir);
